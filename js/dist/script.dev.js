f33ef7aceb387a347fe040654c4fb1b1de43c55b
Zamir <mirmir@.mail.ru>
2022-01-01 20:55:38 +0300

    first commit

---
+++ js/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var number = 5;
+var leftBorder = 1;
+var obj = {
+  a: 50
+};
+obj.a = 1000000;
+console.log(obj);
---
+++ js/script.js
@@ -0,0 +1,18 @@
+let number=5;
+const leftBorder=1;
+
+// const obj = {
+//     a: 50
+// };
+// obj.a = 1000000;
+// console.log(obj);
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = prompt("Вам есть 18?")
+// console.log(typeof(answer + 5));
+const answers=[];
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваше фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
