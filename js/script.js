706d4006ad9212a2517ebcb367b5dc4c45f84f02
Zamir <mirmir@.mail.ru>
2022-01-05 15:17:00 +0300

    personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-let number=5;
-const leftBorder=1;
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
+
+// const leftBorder=1;
 
 // const obj = {
 //     a: 50
@@ -10,9 +29,10 @@ const leftBorder=1;
 // console.log(result);
 // const answer = prompt("Вам есть 18?")
 // console.log(typeof(answer + 5));
-const answers=[];
-answers[0] = prompt('Как Ваше имя?', '');
-answers[1] = prompt('Как Ваше фамилия?', '');
-answers[2] = prompt('Сколько Вам лет?', '');
+// const answers=[];
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваше фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
 
-document.write(answers);
+// document.write(answers);
+
