0a4abcc981e6501b2a15eb0153354e17d3565bd0
Zamir <mirmir@list.ru>
2022-06-15 06:33:47 +0300

    add folder js and files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из полседних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из полседних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
